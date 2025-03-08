66471d2fba5fd96745cf80a15a0d57dbbd0aa39f
alirezadoostdar <alireza.doostdar@gmail.com>
2025-03-08 23:46:43 +0100

    Add name and price test for product model

---
+++ MySolution/Testing/SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,37 @@
+﻿using SimpleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Tests;
+
+public class ProductTests
+{
+    [Fact]
+    public void CanChangeProductName()
+    {
+        // Arange 
+        var p = new Product { Name = "Test", Price = 100M };
+
+        // Act
+        p.Name = "New Name";
+
+        //Assert
+        Assert.Equal("Test", p.Name);
+    }
+
+    [Fact]
+    public void CanChangeProductPrice()
+    {
+        // Arrange
+        var p = new Product { Name = "Test", Price = 100M };
+
+        // Act
+        p.Price = 200M;
+
+        //Assert
+        Assert.Equal(100M, p.Price);
+    }
+}
