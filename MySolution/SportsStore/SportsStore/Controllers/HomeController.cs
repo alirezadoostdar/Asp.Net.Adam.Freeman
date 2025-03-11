a601d54fe3325431102ce0198b5e2730f7305f85
alirezadoostdar <alireza.doostdar@gmail.com>
2025-03-11 13:59:56 +0100

    add dbcontext and conection string

--- MySolution/SportsStore/SportsStore/Controllers/HomeController.cs
+++ MySolution/SportsStore/SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(repository.Products);
         }
     }
 }
